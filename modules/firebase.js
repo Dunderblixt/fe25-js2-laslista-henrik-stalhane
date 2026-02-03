2c1d8573ab0e49903c36f2d3ad698cdeff6fd354
Dunderblixt <stalhane@gmail.com>
2026-02-03 14:33:52 +0100

    Add files via upload

--- modules/firebase.js
+++ modules/firebase.js
@@ -1,27 +1,37 @@
 const url = 'https://bookread-b2814-default-rtdb.europe-west1.firebasedatabase.app/books.json';
 
 export const getAll = async () =>{
-
-    const response = await fetch(url);
-    if(!response.ok) throw new Error(response.status);
-    
-    const tasks = await response.json();
-    return tasks;
+    try {
+        const response = await fetch(url);
+        if(!response.ok) throw new Error(response.status);
+        
+        const tasks = await response.json();
+        return tasks;
+    } catch (error) {
+        alert("Error loading booklist");
+        throw error;
+    }
 }
 
+
 export const addBook = async (book, author) => {
-    const newBook = {book, title: book, isFavorite: false, author: author};
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(newBook),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
+    try {
+        const newBook = {book, title: book, isFavorite: false, author: author};
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(newBook),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
         }
+        
+        const response = await fetch(url, options);
+        if(!response.ok) throw new Error(response.status);
+        const newID = await response.json();
+        console.log(newID.name); //firebase skickar tillbaka id:t som genererades
+        return {id: newID.name, book, isFavorite: false, author: author};
+    } catch (error) {
+        alert("Error adding book");
+        throw error;
     }
-    
-    const response = await fetch(url, options);
-    if(!response.ok) throw new Error(response.status);
-    const newID = await response.json();
-    console.log(newID.name); //firebase skickar tillbaka id:t som genererades
-    return {id: newID.name, book, isFavorite: false, author: author};
 }
